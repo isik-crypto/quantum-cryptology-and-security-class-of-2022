9e8b9cca4c6013a13f7eedbb42a20cc3441f2466
SUBHA KAR <81683258+subha-1997@users.noreply.github.com>
2021-10-22 18:25:07 +0530

    Assignment-1

---
+++ CRS-2021/matrixinverse.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<math.h>
+#include <stdlib.h>
+
+int main()
+{
+	float a[10][10], x[10], r;
+	int i,j,k,n;
+	printf("Enter order of matrix: ");
+	scanf("%d", &n);
+	/* 2. Reading Matrix */
+	printf("Enter the entry of Matrix:\n");
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			printf("a[%d][%d] = ",i,j);
+			scanf("%f", &a[i][j]);
+		}
+	}
+	/* Augmenting Identity Matrix of Order n */
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(i==j)
+			{
+		        	a[i][j+n] = 1;
+			}
+			else
+			{
+				a[i][j+n] = 0;
+			}
+		}
+	}
+	/* Applying Gauss  Elimination */
+	for(i=1;i<=n;i++)
+	{
+		if(a[i][i] == 0.0)
+		{
+			printf("sorry! Input matrix is not invertable");
+			exit(0);
+		}
+		for(j=1;j<=n;j++)
+		{
+			if(i!=j)
+			{
+				r= a[j][i]/a[i][i];
+				for(k=1;k<=2*n;k++)
+				{
+					a[j][k] = a[j][k] - r*a[i][k];
+				}
+			}
+		}
+	}
+	/* Row Operation to Make Principal Diagonal to 1 */
+	for(i=1;i<=n;i++)
+	{
+		for(j=n+1;j<=2*n;j++)
+		{
+			a[i][j] = a[i][j]/a[i][i];
+		}
+	}
+	/* Displaying Inverse Matrix */
+	printf("\nInverse Matrix is:\n");
+	for(i=1;i<=n;i++)
+	{
+		for(j=n+1;j<=2*n;j++)
+		{
+			printf("%0.3f\t",a[i][j]);
+		}
+		printf("\n");
+	}
+	return(0);
+}
