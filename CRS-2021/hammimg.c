5d08334e62d51b2429f47cab713f0784ebf9a274
SUBHA KAR <81683258+subha-1997@users.noreply.github.com>
2022-02-12 19:41:10 +0530

    Add files via upload

---
+++ CRS-2021/hammimg.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+void encoding(char M[], char C[])
+{
+	C[2] = M[0]; C[4] = M[1]; C[5] = M[2]; C[6] = M[3]; //This is encoding of (7,4) Hamming code using the generating matrix
+	C[0] = ((M[0]^M[1])^M[3]);
+	C[1] = (M[0]^M[2])^M[3];
+	C[3] = (M[1]^M[2])^M[3];	
+}
+void error(char C[], char C1[])
+{
+	unsigned int seed;  
+	printf("enter seed value:\n");
+	scanf("%u", &seed);
+	srand(seed);
+	int i = abs(rand())%7;	
+	C1[0] = C[0]; C1[1] = C[1]; C1[2] = C[2]; C1[3] = C[3]; C1[4] = C[4]; C1[5] = C[5]; C1[6] = C[6];
+	C1[i] = abs((C1[i]+1)%2);
+}
+	
+void decoding(char C1[], char M[])
+{
+	char P[3], C[7];
+	P[0] = ((C1[0]^C1[2])^C1[4])^C1[6];  P[1] = ((C1[1]^C1[2])^C1[5])^C1[6];  P[2] = ((C1[3]^C1[4])^C1[5])^C1[6];
+	C[0] = C1[0]; C[1] = C1[1];   C[2] = C1[2]; C[3] = C1[3];   C[4] = C1[4]; C[5] = C1[5]; C[6] = C1[6];
+	int i = (P[0]) + (P[1]*2) + (P[2]*4) -1;	
+	printf("position of the error in the recieved code word: %d \n", i);
+	C[i] = abs((1+C[i])%2);
+	M[0] = C[2]; M[1] = C[4]; M[2] = C[5]; M[3] = C[6]; 	
+}		
+void main()
+{
+	char M[4], C[7], C1[7], M1[4], m;
+	printf("enter a number between 0 and 15: ");
+	scanf("%hhu", &m);
+	printf("\n");  
+  	for(int i=0; i<4; ++i)
+  	{
+  		M[i] = abs(m%2);
+  		m = (m-M[i])/2;
+  	}
+	printf("message: \n");
+  	for(int i=0; i<4; ++i)
+  	{
+  		printf("%hhu ", M[i]);
+  	}
+  	printf("\n");	
+  
+  	encoding(M, C);
+  	printf("code word: \n");
+   	for(int i=0; i<7; ++i)
+   	{
+  		printf("%hhu ", C[i]);
+  	}
+  	printf("\n");
+  
+  	error(C, C1);
+  	printf("recieved word with 1 error: \n");
+  	for(int i=0; i<7; ++i)	
+  	{
+  		printf("%hhu ", C1[i]);
+  	}
+  	printf("\n");
+  
+  	decoding(C1, M1);
+  	printf("decoded message: \n");
+  	for(int i=0; i<4; ++i)
+  	{
+  		printf("%hhu ", M1[i]);
+  	}
+  	printf("\n");
+}
